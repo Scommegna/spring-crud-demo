f3a38a7b47a88c6f10fa8763e3fd898683f84ac6
Scommegna <scommegnal@hotmail.com>
2026-01-20 20:11:20 -0300

    feat: created student with commandLineRunner

--- src/main/java/com/sco/cruddemo/CruddemoApplication.java
+++ src/main/java/com/sco/cruddemo/CruddemoApplication.java
@@ -1,5 +1,7 @@
 package com.sco.cruddemo;
 
+import com.sco.cruddemo.dao.StudentDAO;
+import com.sco.cruddemo.entity.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,9 +15,15 @@ public class CruddemoApplication {
 	}
 
     @Bean
-    public CommandLineRunner commandLineRunner(String[] args) {
+    public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
         return runner -> {
-          System.out.println("Hello");
+            createStudent(studentDAO);
         };
+    }
+
+    private void createStudent(StudentDAO studentDAO) {
+        Student tempStudent = new Student("Lucas", "Scommegna", "email@example.com");
+
+        studentDAO.save(tempStudent);
     }
 }
